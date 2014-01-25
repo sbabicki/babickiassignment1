bd297e132e24d5d89d85091f7fef39d8d8f4c4af
Sasha Babicki <sbabicki@gmail.com>
2014-01-25 16:10:12 -0700

    fixed crash by changing manifest

--- gen/com/example/babickiassignment1/R.java
+++ gen/com/example/babickiassignment1/R.java
@@ -34,7 +34,8 @@ public final class R {
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int counters=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
