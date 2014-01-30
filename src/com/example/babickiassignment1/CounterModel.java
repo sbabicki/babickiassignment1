15138652626f070c6599483dc4cbde998e8d7bcb
Sasha Babicki <sbabicki@gmail.com>
2014-01-29 19:05:29 -0700

    finished CounterModel comments

--- src/com/example/babickiassignment1/CounterModel.java
+++ src/com/example/babickiassignment1/CounterModel.java
@@ -3,6 +3,12 @@ package com.example.babickiassignment1;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/* CounterModel
+ * - Object to store counters -> stores name & date info
+ * - Stores date information in an arraylist of CounterDates
+ * - Each time a CounterDate is added to the arraylist the count is increased
+ * - A string must be passed to the constructor to create a new CounterModel 
+ */
 public class CounterModel implements Serializable{
 
 	// name of counter
@@ -18,10 +24,12 @@ public class CounterModel implements Serializable{
 		this.datesList = new ArrayList<CounterDate>();
 	}
 	
+	// add a CounterDate to the arraylist
 	public void addCount(){
 		datesList.add(new CounterDate());
 	}
 	
+	// the size of the array = # counts
 	public int getCount(){
 		return datesList.size();
 	}
